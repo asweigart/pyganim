03a992938c1a3be959ca798207659e869862b89b
Guillaume Pasquet <guillaume@board.home>
2017-10-21 23:11:58 +0100

    Ensure spritesheet rects are initialized
    When argType is width/height or rows/cols, then
    initialize rects to make sure we don't try to append
    to None.

--- pyganim/__init__.py
+++ pyganim/__init__.py
@@ -73,6 +73,7 @@ def getImagesFromSpriteSheet(filename, width=None, height=None, rows=None, cols=
     sheetImage = pygame.image.load(filename)
 
     if argsType == 'width/height':
+        rects = []
         for y in range(0, sheetImage.get_height(), (sheetImage.get_height() // height)):
             if y + height > sheetImage.get_height():
                 continue
@@ -83,6 +84,7 @@ def getImagesFromSpriteSheet(filename, width=None, height=None, rows=None, cols=
                 rects.append((x, y, width, height))
 
     if argsType == 'rows/cols':
+        rects = []
         spriteWidth = sheetImage.get_width() // cols
         spriteHeight = sheetImage.get_height() // rows
 
